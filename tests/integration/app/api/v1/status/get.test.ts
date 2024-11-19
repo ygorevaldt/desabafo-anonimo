cd67e2edc4eca36c09db8ded3d3bb8c3bac6bd3b
ygorevaldt <evaldtygor@gmail.com>
2024-11-19 14:33:33 -0300

    finaliza endpoint api/v1/status

--- tests/integration/app/api/v1/status/get.test.ts
+++ tests/integration/app/api/v1/status/get.test.ts
@@ -1,9 +1,19 @@
+import { HttpStatusCode } from "@/app/api/constants/http-status-code";
 import axios from "axios";
 import { describe, expect, it } from "vitest";
 
 describe("status", () => {
-  it("should return http status code 200", async () => {
-    const response = await axios.get("http://localhost:3000/api/v1/status");
-    expect(response.status).toBe(200);
+  it("should return data from api status", async () => {
+    const { status, data } = await axios.get(
+      "http://localhost:3000/api/v1/status",
+    );
+
+    const updatedAtParser = new Date(data.updated_at).toISOString();
+
+    expect(status).toEqual(HttpStatusCode.OK);
+    expect(data.updated_at).toEqual(updatedAtParser);
+    expect(data.database.version).toEqual("17.1");
+    expect(data.database.max_connections).toEqual(100);
+    expect(data.database.opened_connections).toBeLessThanOrEqual(10);
   });
 });
