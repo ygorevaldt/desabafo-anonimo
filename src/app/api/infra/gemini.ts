9dc76b97c0fa931d1fe76ae9a72aaf81202fb5f3
ygorevaldt <evaldtygor@gmail.com>
2024-11-28 09:42:29 -0300

    refatora recurso de infra gemini para seguir uma interface de abstração utilizada nas regras de negocio da aplicação

--- src/app/api/infra/gemini.ts
+++ src/app/api/infra/gemini.ts
@@ -1,13 +1,20 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+import { IaProvider } from "./ia-provider.interface";
+
 const googleGenerativeAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const googleGenerativeModel = googleGenerativeAi.getGenerativeModel({
   model: process.env.GEMINI_LANGUAGE_MODEL!,
 });
 
-export const gemini = {
-  async generateResponse(prompt: string) {
-    const { response } = await googleGenerativeModel.generateContent(prompt);
+export class Gemini implements IaProvider {
+  private generativeAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+  private generativeModel = this.generativeAi.getGenerativeModel({
+    model: process.env.GEMINI_LANGUAGE_MODEL!,
+  });
+
+  public async generateResponse(prompt: string): Promise<string> {
+    const { response } = await this.generativeModel.generateContent(prompt);
     return response.text();
-  },
-};
+  }
+}
