8c6103ed483565a3f288d1ffe45c32572508780b
ygorevaldt <evaldtygor@gmail.com>
2024-11-28 09:50:23 -0300

    remove variaveis desnecessárias

--- src/app/api/infra/gemini.ts
+++ src/app/api/infra/gemini.ts
@@ -2,18 +2,13 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 import { IaProvider } from "./ia-provider.interface";
 
-const googleGenerativeAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
-const googleGenerativeModel = googleGenerativeAi.getGenerativeModel({
-  model: process.env.GEMINI_LANGUAGE_MODEL!,
-});
-
 export class Gemini implements IaProvider {
   private generativeAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
   private generativeModel = this.generativeAi.getGenerativeModel({
     model: process.env.GEMINI_LANGUAGE_MODEL!,
   });
 
-  public async generateResponse(prompt: string): Promise<string> {
+  async generateResponse(prompt: string): Promise<string> {
     const { response } = await this.generativeModel.generateContent(prompt);
     return response.text();
   }
