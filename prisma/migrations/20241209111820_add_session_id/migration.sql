7b0d7413faf1159420a8720a3a7b33ddbe3537ea
ygorevaldt <evaldtygor@gmail.com>
2024-12-09 08:19:15 -0300

    adiciona a soluna session_id a tabela de apoios

---
+++ prisma/migrations/20241209111820_add_session_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - Added the required column `session_id` to the `apoio` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "apoio" ADD COLUMN     "session_id" UUID NOT NULL;
