dbea4f97a81780b8bbe8c680a2b08633e3dccb43
ygorevaldt <evaldtygor@gmail.com>
2024-12-12 05:35:30 -0300

    instala biblioteca sweetalert para alertas customizados

---
+++ src/utils/alert.ts
@@ -0,0 +1,38 @@
+import Swal from "sweetalert2";
+
+const toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  showCloseButton: true,
+  timer: 2000,
+  timerProgressBar: true,
+  customClass: {
+    title: "notification-service-title",
+  },
+  didOpen: (toast) => {
+    toast.onmouseenter = Swal.stopTimer;
+    toast.onmouseleave = Swal.resumeTimer;
+  },
+});
+
+export async function successAlert(message: string) {
+  await toast.fire({
+    icon: "success",
+    title: message,
+  });
+}
+
+export async function errorAlert(message: string) {
+  await toast.fire({
+    icon: "error",
+    title: message,
+  });
+}
+
+export async function infoAlert(message: string) {
+  await toast.fire({
+    icon: "info",
+    title: message,
+  });
+}
