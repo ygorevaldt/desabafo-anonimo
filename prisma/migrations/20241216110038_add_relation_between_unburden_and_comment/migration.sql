79d469c3367f55af481bd71d8ac144f7c70365f8
ygorevaldt <evaldtygor@gmail.com>
2024-12-19 17:30:22 -0300

    corrige bug nas migrations

--- prisma/migrations/20241216110038_add_relation_between_unburden_and_comment/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "apoio" ALTER COLUMN "session_id" SET DEFAULT gen_random_uuid()::TEXT;
-
--- AlterTable
-ALTER TABLE "comentario" ADD COLUMN     "id_desabafo" UUID;
-
--- AddForeignKey
-ALTER TABLE "comentario" ADD CONSTRAINT "comentario_id_desabafo_fkey" FOREIGN KEY ("id_desabafo") REFERENCES "desabafo"("id") ON DELETE SET NULL ON UPDATE CASCADE;
